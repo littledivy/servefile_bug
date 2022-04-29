02c6ac5053d1b254da3fd074a7da3fc4b28b4216
Divy Srivastava <dj.srivastava23@gmail.com>
2022-04-29 10:58:42 +0530

    Update main.ts

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@ import { serveFile } from "https://deno.land/std@0.136.0/http/file_server.ts";
 import { serve } from "https://deno.land/std@0.136.0/http/server.ts";
 
 async function server(request: Request): Promise<Response> {
-  return await serveFile(request, "/index.html");
+  return await serveFile(request, "/src/index.html");
 }
 
 await serve(server);
